56ab198269d3da19fc006b251075cd8868e9e629
Lennard Grabowski <106014763+LennardKnorke@users.noreply.github.com>
2023-01-24 20:21:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-#%%
 from PIL import Image
 import os
 import numpy as np
 from tqdm import tqdm
 from sklearn.neighbors import NearestNeighbors
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-def choose_image_folder(data_base_exists):
-    if data_base_exists == False:
-        return "new"
-    print(os.listdir("Image Database"))
-    while True:
-        folder_choice = input("choose an available folder create a 'new' folder")
-        if folder_choice == "new" or folder_choice == "New":
-            return "new"
-        elif folder_choice in list(os.listdir('Image Database/')):
-            return folder_choice
-        else:
-            print("Invalid choice")
-
+######Functions
+#Checks if the folder with the filler images exits. If it doesnt, it also creates it
 def check_database_exists():
     if os.path.exists('Image database') == False:
         os.mkdir('Image database')
         return False
     else:
         return True
+    
+#User input whether a pregiven folder is to be picked, or a new library is created
+def choose_image_folder(data_base_exists:bool):
+    if data_base_exists == False:
+        return "new"
+    print(os.listdir("Image Database"))
+    while True:
+        folder_choice = input("choose an available folder, or create a 'new' folder: ")
+        if folder_choice == "new" or folder_choice == "New":
+            return "new"
+        elif folder_choice in list(os.listdir('Image Database/')):
+            return folder_choice
+        else:
+            print("Invalid choice!")
+
+def fetch_google_pics(keyword:str, name:str):
+    API_KEY = "AIzaSyAIzp4BPjJbmikhQbzQIDbSymLZ_XBZW30"#oha du
+    max_results = 100
+    os.mkdir(f"Image Database/{name}")
 def choose_crop_size():
     while True:
         crop_size = input("Choose 1x1, 2x2, 5x5 or 10x10 crop area. Smaller choices result in higher quality but increased memory space and processing time")
@@ -35,8 +44,10 @@ def choose_crop_size():
             return 5
         elif crop_size == "10x10" or crop_size == "10":
             return 10
+        else:
+            print("Invalid Choice")
 
-def pre_process_smaller_pictures(folder, sub_pic_size):
+def pre_process_smaller_pictures(folder:str, sub_pic_size:int):
     print("Preprocessing Pictures")
     picture_list = os.listdir(f"Image database/{folder}")
     fill_RGB = []
@@ -50,13 +61,13 @@ def pre_process_smaller_pictures(folder, sub_pic_size):
     fill_RGB = np.array(fill_RGB, dtype=float)
     return fill_RGB
 
-def fit_model(rgb_list):
+def fit_model(rgb_list:list):
     fill_knn = NearestNeighbors(n_neighbors=1)
     fill_knn.fit(rgb_list)
     return fill_knn
 
-def fetch_google_pics(keyword):
-    pass
+
+
 
 def find_og():
     while True:
@@ -66,7 +77,7 @@ def find_og():
         elif name+'.jpg' in os.listdir():
             return name+'.jpg'
 
-def open_and_adjust_picture(file_name):
+def open_and_adjust_picture(file_name:str):
     picture = Image.open(file_name)
     while True:
         #possibility to standardize picture
@@ -148,20 +159,31 @@ def create_new_picture(og_picture, subpicSize, segSize, knn_model, small_picture
 ###Main Loop
 if __name__ == "__main__":
     RUNNING = True
-    data_exists = check_database_exists()
+    folder_exists = check_database_exists()
     while RUNNING:
-        small_folder = choose_image_folder(data_base_exists= data_exists)
+        small_folder = choose_image_folder(data_base_exists = folder_exists)
 
         #Create a new image database to use
         if small_folder == "new":
-            fetch_google_pics("lol")
-            print("WORK IN PROGRESS")
-            break
+            new_base_checkpoint = True
+            download_orexisting = None
+            while new_base_checkpoint:
+                download_orexisting = input("Do you want to use an existing library (0) or download one using a keyword (1)?: ")
+                if int(download_orexisting) == 0 or int(download_orexisting) == 1:
+                    new_base_checkpoint = False
+                else:
+                    print("Invalid Choice")
+            if int(download_orexisting) == 0:
+                pass
+            else:
+                search_key_term = input("What kind of keyword are we looking for?: ")
+                lib_name = input("Name the new library: ")
+                fetch_google_pics(search_key_term, lib_name)
         #Internal loop. Gets user input for the cropping resolution
         og_crop_size = choose_crop_size()
 
         #Choose size of the filling pictures replacing a croped area
-        small_pictures_size = input("Choose the size of the filling pictures")
+        small_pictures_size = input("Size of the small filling pictures (recommended 50 - 100)")
         small_pictures_size = int(small_pictures_size)
 
         all_pics_rgbvals = pre_process_smaller_pictures(small_folder, small_pictures_size)
@@ -173,5 +195,5 @@ if __name__ == "__main__":
         create_new_picture(og_picture=og_picture, subpicSize = small_pictures_size, segSize = og_crop_size, knn_model = model, small_picture_list = all_pics_rgbvals, selected_folder = small_folder)
 
         doagain = input("enter 'exit' to exit the programm or any input to try again")
-        if  doagain == "exit" or doagain == "Exit":
+        if doagain == "exit" or doagain == "Exit":
             RUNNING = False
